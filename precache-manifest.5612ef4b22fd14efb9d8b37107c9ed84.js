c6aefec6802bdb3ffe4e5c711299734b2b34fc59
ChanakaA <chanakaathurugiriya@gmail.com>
2019-04-24 21:01:30 +0530

    Updates

---
+++ precache-manifest.5612ef4b22fd14efb9d8b37107c9ed84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2bd2fda1b5cb50a693f5dc0d5866cca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14f5131d6eb094803274",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "14f5131d6eb094803274",
+    "url": "/static/js/main.dc9da1ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
